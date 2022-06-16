d62549df64d91b0202d511797b96117799b304b4
thomasjoyce1999 <trj1999@gmail.com>
2022-06-16 15:19:21 -0400

    updated classes and models

---
+++ template-csharp-reviews-site/template-csharp-reviews-site/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿namespace template_csharp_reviews_site.Models
+{
+    public class Review
+    {
+        public int Id;
+        public int Rating;
+        public string Opinions;
+        public DateTime DateTime;
+        public bool Positive;
+        public string Name;
+        public Review(int id, int rating, string opinions, DateTime dateTime, bool positive, string name)
+        {
+            Id = id;
+            Rating = rating;
+            Opinions = opinions;
+            DateTime = dateTime;
+            Positive = positive;
+            Name = name;
+        }
+    }
+   
+}
---
+++ template-csharp-reviews-site/template-csharp-reviews-site/Models/SchoolSupplies.cs
@@ -0,0 +1,22 @@
+﻿namespace template_csharp_reviews_site.Models
+{
+    public class SchoolSupplies
+    {
+        public int Id;
+        public string Name;
+        public string Description;
+        public double Price;
+        public SupplyType SupplyType;
+        public AgeGroup AgeGroup;
+            
+    }
+    public enum AgeGroup
+    { Pre_K, ElemtrySchool, IdermiditeSchool, MiddleSchool, HighSchool, College
+    }
+    public enum SupplyType
+    {
+        Writing,Books,Binders,Calculator,Notebooks,Paper,Backpacks
+    }
+
+
+}
