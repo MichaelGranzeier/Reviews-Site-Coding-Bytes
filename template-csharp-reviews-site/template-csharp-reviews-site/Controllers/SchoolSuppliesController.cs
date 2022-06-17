b3cb5f177f6173c972bbce19e7f417c049169b56
MichaelGranzeier <granzeierpeter@gmail.com>
2022-06-17 12:03:30 -0400

    added Controller and model

--- template-csharp-reviews-site/template-csharp-reviews-site/Controllers/SchoolSuppliesController.cs
+++ template-csharp-reviews-site/template-csharp-reviews-site/Controllers/SchoolSuppliesController.cs
@@ -5,10 +5,19 @@ namespace template_csharp_reviews_site.Controllers
 {
     public class SchoolSuppliesController : Controller
     {
-        private List<SchoolSupplies> SchoolSuppliesList = new List<SchoolSupplies>() { new SchoolSupplies() { Id = 1,Name = "Pens"},new SchoolSupplies() { Id = 2, Name = "Pencils" } };
+        private List<SchoolSupplies> SchoolSuppliesList = new List<SchoolSupplies>()
+        { 
+            new SchoolSupplies() { Id = 1, Name = "Writing"},
+            new SchoolSupplies() { Id = 2, Name = "Books"},
+            new SchoolSupplies() { Id = 3, Name = "Notebooks"},
+            new SchoolSupplies() { Id = 4, Name = "Calculators"},
+            new SchoolSupplies() { Id = 5, Name = "Backpacks"},
+            new SchoolSupplies() { Id = 6, Name = "Binders"},
+            new SchoolSupplies() { Id = 7, Name = "Paper"}
+        };
         public IActionResult Index()
         {
-            return View();
+            return View(SchoolSuppliesList);
         }
     }
 }
