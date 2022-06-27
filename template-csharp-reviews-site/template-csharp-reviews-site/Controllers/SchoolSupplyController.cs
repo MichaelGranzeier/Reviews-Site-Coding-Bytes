d57acc10133f011e273643620098c2b57b26f32b
thomasjoyce1999 <trj1999@gmail.com>
2022-06-27 15:58:15 -0400

    updated 6/27/2022

--- template-csharp-reviews-site/template-csharp-reviews-site/Controllers/SchoolSupplyController.cs
+++ template-csharp-reviews-site/template-csharp-reviews-site/Controllers/SchoolSupplyController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using template_csharp_reviews_site.Models;
 
 namespace template_csharp_reviews_site.Controllers
@@ -16,5 +17,58 @@ namespace template_csharp_reviews_site.Controllers
         {
             return View(db.SchoolSupplies.ToList());
         }
+
+        public IActionResult Details(int id)
+        {
+            return View(db.SchoolSupplies.Find(id));
+        }
+
+        public IActionResult Create() 
+        {
+            List<object> enums = new List<object>()
+            {
+                new {Id = AgeGroup.Pre_K, Name="Pre K"},
+                new {Id = AgeGroup.ElemSchool, Name="Elem School"},
+                new {Id = AgeGroup.MiddleSchool, Name="Middle School"},
+                new {Id = AgeGroup.HighSchool, Name="High School"},
+                new {Id = AgeGroup.College, Name="College"}
+            };
+            ViewBag.Ages = new SelectList(enums, "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(SchoolSupply model)
+        {
+            db.SchoolSupplies.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            ViewBag.SchoolSupplies = new SelectList(db.Reviews.ToList(), "Id", "Name");
+            return View(db.Reviews.Find(id));
+        }
+        [HttpPost]
+        public IActionResult Edit(SchoolSupply model)
+        {
+            db.SchoolSupplies.Update(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            SchoolSupply Schoolsupply = db.SchoolSupplies.Find(id);
+            return View(Schoolsupply);
+        }
+        [HttpPost]
+        public IActionResult Delete(SchoolSupply model)
+        {
+            db.SchoolSupplies.Remove(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+       
+        
     }
 }
